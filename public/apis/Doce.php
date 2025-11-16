a22fa5f6b32124e7f6113cb39d6cc0851d24670a
Bia-Casa <biagsantana1000@gmail.com>
2025-11-16 15:35:16 -0300

    arrumando api

--- public/apis/Doce.php
+++ public/apis/Doce.php
@@ -12,29 +12,29 @@
 
     if(!empty($categoria)) {
         //doces de uma categoria específica
-        $sql = "select * from doces where ativo = 'S' and categoria_id = :categoria order by nome";
+        $sql = "select * from doce where ativo = 'S' and categoria_id = :categoria order by nome";
         $consulta = $pdo->prepare($sql);
         $consulta->bindParam(':categoria', $categoria);
         $consulta->execute();
 
-        $dadosDoces = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        $dadosDoce = $consulta->fetchAll(PDO::FETCH_ASSOC);
     } else if(!empty($id)) {
         //doce em específico pelo id
-        $sql = "select * from doces where ativo = 'S' and id_doces = :id limit 1";
+        $sql = "select * from doce where ativo = 'S' and id = :id limit 1";
         $consulta = $pdo->prepare($sql);
         $consulta->bindParam(':id', $id);
         $consulta->execute();
 
-        $dadosDoces = $consulta->fetch(PDO::FETCH_ASSOC);
+        $dadosDoce = $consulta->fetch(PDO::FETCH_ASSOC);
 
     } else {
         //todos os doces
-         $sql = "select * from doces where ativo = 'S' and order by nome";
+         $sql = "select * from doce where ativo = 'S' order by nome";
         $consulta = $pdo->prepare($sql);
         $consulta->execute();
 
-        $dadosDoces = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        $dadosDoce = $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    echo json_encode($dadosDoces);
+    echo json_encode($dadosDoce);
 ?>
