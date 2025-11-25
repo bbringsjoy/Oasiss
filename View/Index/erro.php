893c7e308752fd740d55b716e8d39346dcba0760
Francesco Gris <francescofgris@gmail.com>
2025-11-25 00:29:39 -0300

    front_do_erro'

--- View/Index/erro.php
+++ View/Index/erro.php
@@ -1,7 +1,7 @@
 <div class="card">
     <p class="text-center" style="background-color: var(--maincolor); margin: 0; padding-bottom:20px">
         <img src="images/404.png" alt="Não encontrado!" style="width: 100%;">
-    <button class="btn btnqrt" style="border-radius: 10px;">Home
+    <button class="btn btnqrt" style="border-radius: 10px;margin-top:20px; font-weight:bold">Home
     </button>
     </p>
 </div>
